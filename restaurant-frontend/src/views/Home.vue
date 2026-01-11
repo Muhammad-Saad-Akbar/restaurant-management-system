7d642a3b421533309877c2f958379832e0098861
SaadAkbarGithub <123985914+Muhammad-Saad-Akbar@users.noreply.github.com>
2026-01-12 03:31:59 +0500

    Made some changes

--- restaurant-frontend/src/views/Home.vue
+++ restaurant-frontend/src/views/Home.vue
@@ -94,62 +94,22 @@
       </div>
 
       <!-- Cards -->
-      <div v-if="restaurants.length > 0">
-        <div class="flex flex-col px-2.5 sm:items-center lg:items-start md:px-0 2xl:grid grid-cols-2 gap-7.5 sm:gap-10 md:gap-11 lg:gap-13">
-          <div class="mt-1 w-full sm:max-w-sm md:mt-0 md:max-w-xl lg:max-w-2xl bg-white rounded-2xl shadow-lg overflow-hidden relative" v-for="restaurant in restaurants"
-               :key="restaurant.id">
-
-            <button type="button" class="absolute top-4 right-4 text-gray-400 hover:text-red-500 transition-colors duration-300" @click="deleteRestaurant(restaurant.id)">
-              <i class="ri-delete-bin-6-line text-xl"></i>
-            </button>
-
-            <div class="flex flex-col md:flex-row">
-              <div class="w-full md:w-1/3">
-                <img class="object-cover h-52 w-full md:h-full" :src="restaurant.logo" alt="Restaurant Img">
-              </div>
-              <div class="w-full md:w-2/3 p-6 pb-4.5 md:p-8">
-                <p class="text-sm text-gray-500 font-medium">{{ restaurant.regNumber }}</p>
-                <h2 class="text-2xl md:text-3xl font-bold text-gray-800 mt-1">{{ restaurant.restaurantName }}</h2>
-
-                <div class="mt-6 space-y-4 text-gray-700">
-                  <div class="flex items-start">
-                    <i class="fas fa-map-marker-alt w-5 mt-1 text-red-500"></i>
-                    <div class="ml-3">
-                      <h4 class="font-semibold">Address</h4>
-                      <p class="text-gray-600 text-sm md:text-base">{{ restaurant.restaurantAddress }}</p>
-                    </div>
-                  </div>
-                  <div class="flex items-center">
-                    <i class="fas fa-phone-alt w-5 text-red-500"></i>
-                    <div class="ml-3">
-                      <h4 class="font-semibold">Phone</h4>
-                      <p class="text-gray-600 text-sm md:text-base">{{ restaurant.tel }}</p>
-                    </div>
-                  </div>
-                  <div class="flex items-center">
-                    <i class="fas fa-clock w-5 text-red-500"></i>
-                    <div class="ml-3">
-                      <h4 class="font-semibold">Hours</h4>
-                      <p class="text-gray-600 text-sm md:text-base">{{ restaurant.openingHours }} (Opening) - {{ restaurant.closingHours }} (Closing)</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="mt-6.5 md:mt-8 text-right">
-                  <button type="button" class="bg-red-500 text-white font-semibold px-8 py-2 rounded-full hover:bg-red-600 transition-colors duration-300"
-                          @click="editRestaurant(restaurant.id)">
-                    Edit
-                  </button>
-                </div>
+      <Suspense>
+        <AsyncComponent />
+        <template #fallback>
+          <div class="flex items-center justify-center h-40">
+            <div class="relative w-20 h-20">
+              <div class="absolute inset-0 rounded-full border-4 border-gray-300"></div>
+              <div class="absolute inset-0 rounded-full border-4 border-transparent border-t-red-500 animate-spin"></div>
+              <div class="absolute inset-0 flex items-center justify-center text-sm font-semibold text-gray-700">
+                Loading...
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div v-else>
-        <p class="font-serif-display text-gray-500 text-lg font-bold">No restaurants have been added yet</p>
-      </div>
+        </template>
+      </Suspense>
     </main>
+
   </div>
 </template>
 
@@ -157,17 +117,17 @@
 import Swal from 'sweetalert2';
 import { createApp } from "vue";
 import RestaurantForm from "@/components/AddRestaurantForm.vue";
-import EditRestaurantForm from "@/components/EditRestaurantForm.vue";
 import axios from "axios";
 import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 import BASE_URL from "@/api/api.js"
+import AsyncComponent from "@/components/AsyncComponent.vue";
 
 export default {
   name: "Home",
+  components: {AsyncComponent},
   data() {
     return {
-      restaurants: [],
       userName: null
     }
   },
@@ -180,13 +140,13 @@ export default {
     }
     this.userName = user.name
     console.log(user)
-    this.restaurantData()
   },
   methods: {
     addRestaurant() {
       let formApp;
       let formInstance;
       const vm = this;
+
       Swal.fire({
         title: 'Add New Restaurant',
         html: `<div id="vue-form"></div>`,
@@ -231,7 +191,7 @@ export default {
       }).then((result) => {
         if (result.isConfirmed && result.value.status === 201) {
           console.log("Restaurant Added Successfully", result);
-          vm.restaurantData()
+          // vm.restaurantData()
           toast.success(' New Restaurant Successfully Added', {
             autoClose: 2200,
             style: {
@@ -248,109 +208,6 @@ export default {
       const randomNum = Math.floor(Math.random() * 1000);
       return `${prefix}-${randomNum}`;
     },
-    editRestaurant(id) {
-      let formApp;
-      let formInstance;
-      const vm = this;
-
-      Swal.fire({
-        title: "Edit Restaurant",
-        html: `<div id="vue-edit-form"></div>`,
-        showCancelButton: true,
-        confirmButtonText: "Update",
-        cancelButtonText: "Cancel",
-        allowOutsideClick: false,
-        focusConfirm: false,
-        didOpen() {
-          formApp = createApp(EditRestaurantForm, { restaurantId: id });
-          formInstance = formApp.mount("#vue-edit-form");
-
-          const confirmBtn = Swal.getConfirmButton();
-          if (confirmBtn) {
-            confirmBtn.setAttribute("type", "button");
-          }
-        },
-        async preConfirm() {
-          return await axios.put(`${BASE_URL}/restaurants/${id}`, {
-            regNumber: formInstance.regNumber,
-            restaurantName: formInstance.restaurantName,
-            ownerName: formInstance.ownerName,
-            tel: formInstance.tel,
-            emailAddress: formInstance.emailAddress,
-            restaurantAddress: formInstance.restaurantAddress,
-            openingHours: formInstance.openingHours,
-            closingHours: formInstance.closingHours,
-            logo: formInstance.selectedFile
-          }).then((response) => {
-            if (response.status === 200) {
-              return {
-                status: response.status,
-                data: response.data
-              };
-            }
-          }).catch((err) => {
-            Swal.showValidationMessage(`Update failed: ${err}`);
-          });
-        },
-        willClose() {
-          formApp.unmount();
-        }
-      }).then((result) => {
-        if (result.isConfirmed && result.value.status === 200) {
-          console.log("Restaurant updated", result);
-          vm.restaurantData();
-          toast.success('Restaurant Successfully updated', {
-            autoClose: 2200,
-            style: {
-              width: '350px'
-            },
-          });
-        }else {
-          console.log("Cancelled", result);
-        }
-      });
-    },
-    deleteRestaurant(id) {
-      const vm = this;
-      Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!",
-        async preConfirm() {
-          return await axios.delete(`${BASE_URL}/restaurants/${id}`).then((response) => {
-            if (response.status === 200) {
-              return response.status;
-            }
-          }).catch((err) => {
-            Swal.showValidationMessage(`Update failed: ${err}`);
-          });
-        }
-      }).then((result) => {
-        if (result.isConfirmed && result.value === 200) {
-          console.log("Restaurant deleted Successfully", result)
-          vm.restaurantData()
-          Swal.fire({
-            title: "Deleted!",
-            text: "Your Restaurant has been deleted.",
-            icon: "success"
-          });
-        }else {
-          console.log("Cancelled", result)
-        }
-      });
-    },
-    async restaurantData() {
-      let result = await axios.get(`${BASE_URL}/restaurants`);
-      if (result.status === 200) {
-        this.restaurants = result.data
-      }else {
-        console.log("Api Call Failed", result);
-      }
-    },
     logout() {
       localStorage.clear();
       this.$router.push({
@@ -364,5 +221,12 @@ export default {
 <style scoped>
 .font-serif-display {
   font-family: 'Lora', serif;
+}
+@keyframes loading {
+  0% { transform: translateX(-100%); }
+  100% { transform: translateX(100%); }
+}
+.animate-\[loading_1\.5s_linear_infinite\] {
+  animation: loading 1.5s linear infinite;
 }
 </style>
